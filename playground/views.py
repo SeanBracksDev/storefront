9b5638c1886c4fc2faac15cd722971aa47fd9e3e
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-05-10 19:43:53 +0100

    remove playground app - was for sandboxing

--- playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-# request -> response
-def say_hello(request):
-    x = 1
-    y = 2
-    return render(request, "hello.html", {"name": "Mosh"})
